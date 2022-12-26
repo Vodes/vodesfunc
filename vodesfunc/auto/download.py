75a1e73e56f6d17c3fccaa269da3d1bf84d51ad6
Vodes <48185123+Vodes@users.noreply.github.com>
2022-12-26 02:01:03 +0100

    Fix downloading of binaries

--- vodesfunc/auto/download.py
+++ vodesfunc/auto/download.py
@@ -18,8 +18,10 @@ types: dict = {
     'qaac': 'https://github.com/nu774/qaac/releases/download/v2.76/qaac_2.76.zip',
     'x265': 'https://github.com/DJATOM/x265-aMod/releases/download/3.5+20/x265-x64-v3.5+20-aMod-gcc10.3.0-opt-znver3.7z',
     'x264': 'https://github.com/DJATOM/x264-aMod/releases/download/r3101%2B20/x264-aMod-x64-core164-r3101+20.7z',
-    'mkvmerge': 'https://www.fosshub.com/MKVToolNix.html?dwl=mkvtoolnix-64-bit-72.0.0.7z',
-    'mkvextract': 'https://www.fosshub.com/MKVToolNix.html?dwl=mkvtoolnix-64-bit-72.0.0.7z'
+    # Before anyone complains, this is the exact same file as on https://www.fosshub.com/MKVToolNix.html?dwl=mkvtoolnix-64-bit-72.0.0.7z
+    # Feel free to compare the hashes; They changed their cloudflare bot settings and wget doesn't work... 
+    'mkvmerge': 'https://files.catbox.moe/5efx9l.7z',
+    'mkvextract': 'https://files.catbox.moe/5efx9l.7z'
 }
 
 def get_executable(type: str, can_download: bool = True) -> str:
@@ -58,6 +60,9 @@ def download_binary(type: str) -> str:
         if exe.is_file():
             executable = exe
 
+    if executable is None:
+        raise f"Binary for '{type}' could not have been found!"
+
     return str(executable.resolve())
 
 
@@ -65,9 +70,13 @@ def unpack_all(dir: str | Path):
     dir = Path(dir) if isinstance(dir, str) else dir
 
     for file in dir.rglob('*.zip'):
-        sh.unpack_archive(file, Path(dir.stem).mkdir(exist_ok=True))
+        out = Path(os.path.join(file.resolve(True).parent, file.stem))
+        out.mkdir(exist_ok=True)
+        sh.unpack_archive(file, out)
         os.remove(file)
 
     for file in dir.rglob('*.7z'):
-        p7z.unpack_7zarchive(file, Path(dir.stem).mkdir(exist_ok=True))
+        out = Path(os.path.join(file.resolve(True).parent, file.stem))
+        out.mkdir(exist_ok=True)
+        p7z.unpack_7zarchive(file, out)
         os.remove(file)
