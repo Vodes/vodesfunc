27366ed06e17898329ffc0ef60f52dd606b39b82
Vodes <48185123+Vodes@users.noreply.github.com>
2023-11-05 23:19:50 +0100

    schizo_denoise: Add bm3dhip fallback

--- vodesfunc/denoise.py
+++ vodesfunc/denoise.py
@@ -1,27 +1,37 @@
 from vstools import vs, core, get_y, get_u, get_v, depth, get_depth, join
 from vsrgtools import contrasharpening
 
-__all__ = ['VMDegrain', 'schizo_denoise']
+__all__ = ["VMDegrain", "schizo_denoise"]
 
-def VMDegrain(src: vs.VideoNode, thSAD: int = 60, prefilter: vs.VideoNode | int = 2, 
-    smooth: bool = True, block_size: int = 32, overlap: int = 16, **kwargs) -> vs.VideoNode:
+
+def VMDegrain(
+    src: vs.VideoNode, thSAD: int = 60, prefilter: vs.VideoNode | int = 2, smooth: bool = True, block_size: int = 32, overlap: int = 16, **kwargs
+) -> vs.VideoNode:
     """
-        Just some convenience function for mvtools with a useable preset and temporal smoothing.
-        Check the MVTools Docs for the params that aren't listed below.
+    Just some convenience function for mvtools with a useable preset and temporal smoothing.
+    Check the MVTools Docs for the params that aren't listed below.
 
 
-        :param src:             Input to denoise
-        :param smooth:          Run TTempsmooth on the denoised clip if True
-        :return:                Denoised clip              
+    :param src:             Input to denoise
+    :param smooth:          Run TTempsmooth on the denoised clip if True
+    :return:                Denoised clip
     """
     from vsdenoise import MVTools, SADMode, SearchMode, MotionMode, PelType, Prefilter
+
     if isinstance(prefilter, int):
         prefilter = Prefilter(prefilter)
     y = depth(get_y(src), 16)
     d_args = dict(
-        prefilter=prefilter, thSAD=thSAD, block_size=block_size, overlap=overlap,
-        sad_mode=SADMode.SPATIAL.same_recalc, search=SearchMode.DIAMOND, 
-        motion=MotionMode.HIGH_SAD, pel_type=PelType.BICUBIC, rfilter=2, sharp=2,
+        prefilter=prefilter,
+        thSAD=thSAD,
+        block_size=block_size,
+        overlap=overlap,
+        sad_mode=SADMode.SPATIAL.same_recalc,
+        search=SearchMode.DIAMOND,
+        motion=MotionMode.HIGH_SAD,
+        pel_type=PelType.BICUBIC,
+        rfilter=2,
+        sharp=2,
     )
     d_args.update(**kwargs)
 
@@ -33,26 +43,35 @@ def VMDegrain(src: vs.VideoNode, thSAD: int = 60, prefilter: vs.VideoNode | int 
     return out if src.format.color_family == vs.GRAY else join(out, src)
 
 
-def schizo_denoise(src: vs.VideoNode, sigma: float | list[float] = [0.8, 0.3], thSAD: int = 60, 
-    radius: int | list[int] = 2, nlm_a: int = 2, prefilter: vs.VideoNode | int = 2, 
-    cuda: bool | list[bool] = True, csharp: int | bool = False, **kwargs) -> vs.VideoNode:
+def schizo_denoise(
+    src: vs.VideoNode,
+    sigma: float | list[float] = [0.8, 0.3],
+    thSAD: int = 60,
+    radius: int | list[int] = 2,
+    nlm_a: int = 2,
+    prefilter: vs.VideoNode | int = 2,
+    cuda: bool | list[bool] = True,
+    csharp: int | bool = False,
+    **kwargs,
+) -> vs.VideoNode:
     """
-        Convenience function for (k)nlm on chroma and mvtools + bm3d(cuda) on luma.
-        Mostly for personal scripts so please don't complain too much unless it's an actual issue.
+    Convenience function for (k)nlm on chroma and mvtools + bm3d(cuda) on luma.
+    Mostly for personal scripts so please don't complain too much unless it's an actual issue.
 
-        :param src:         Input to denoise
-        :param sigma:       Essentially strength for NLMeans and BM3D.
-                            Float or list of floats in this order [bm3d, nlm_uv] or [bm3d, nlm_u, nlm_v]
-        :param thSAD:       Not exactly strength but something like that, for mvtools.
-        :param radius:      Temporal Radius used for NLMeans and BM3D.
-                            Int or list of ints in this order [bm3d, nlm]
-        :param prefilter:   vsdenoise Prefilter or prefiltered clip to use for mvtools.
-                            Defaults to MINBLUR3
-        :param cuda:        Uses NlmCuda and BM3DCuda respectively if available. The latter prefers RTC if available.
-        :param csharp:      Apply contrasharpening after denoising. True defaults to 3 while False obviously disables it.
-        :param kwargs:      Any parameters you might wanna pass to bm3d or mvtools.
+    :param src:         Input to denoise
+    :param sigma:       Essentially strength for NLMeans and BM3D.
+                        Float or list of floats in this order [bm3d, nlm_uv] or [bm3d, nlm_u, nlm_v]
+    :param thSAD:       Not exactly strength but something like that, for mvtools.
+    :param radius:      Temporal Radius used for NLMeans and BM3D.
+                        Int or list of ints in this order [bm3d, nlm]
+    :param prefilter:   vsdenoise Prefilter or prefiltered clip to use for mvtools.
+                        Defaults to MINBLUR3
+    :param cuda:        Uses NlmCuda and BM3DCuda respectively if available. The latter prefers RTC if available.
+                        Will fallback to BM3DHip if installed and no cuda available.
+    :param csharp:      Apply contrasharpening after denoising. True defaults to 3 while False obviously disables it.
+    :param kwargs:      Any parameters you might wanna pass to bm3d or mvtools.
 
-        :return:            Denoised clip
+    :return:            Denoised clip
     """
     if src.format.color_family != vs.YUV:
         raise ValueError("schizo_denoise: This function expects a full YUV clip.")
@@ -68,6 +87,7 @@ def schizo_denoise(src: vs.VideoNode, sigma: float | list[float] = [0.8, 0.3], t
 
     if isinstance(prefilter, int):
         from vsdenoise import Prefilter
+
         prefilter = Prefilter(prefilter)
 
     clip = depth(src, 16)
@@ -75,22 +95,33 @@ def schizo_denoise(src: vs.VideoNode, sigma: float | list[float] = [0.8, 0.3], t
     nlmfunc = core.knlm.KNLMeansCL if not hasattr(core, "nlm_cuda") or not cuda[0] else core.nlm_cuda.NLMeans
 
     if len(sigma) == 3:
-        clip_u = nlmfunc(clip, a=nlm_a, d=radius[1], h=sigma[1], channels='U')
-        clip_v = nlmfunc(clip, a=nlm_a, d=radius[1], h=sigma[2], channels='V')
+        clip_u = nlmfunc(clip, a=nlm_a, d=radius[1], h=sigma[1], channels="U")
+        clip_v = nlmfunc(clip, a=nlm_a, d=radius[1], h=sigma[2], channels="V")
         nlm = join(get_y(clip), get_u(clip_u), get_v(clip_v))
     else:
-        clip_uv = nlmfunc(clip, a=nlm_a, d=radius[1], h=sigma[1], channels='UV')
+        clip_uv = nlmfunc(clip, a=nlm_a, d=radius[1], h=sigma[1], channels="UV")
         nlm = join(clip, clip_uv)
 
     # 'Extract' possible bm3d args before passing kwargs to mvtools :)
-    bm3dargs = dict(block_step=kwargs.pop("block_step", 8), bm_range=kwargs.pop("bm_range", 9),
-        ps_num=kwargs.pop("ps_num", 2), ps_range=kwargs.pop("ps_range", 4), fast=kwargs.pop("fast", True))
+    bm3dargs = dict(
+        block_step=kwargs.pop("block_step", 8),
+        bm_range=kwargs.pop("bm_range", 9),
+        ps_num=kwargs.pop("ps_num", 2),
+        ps_range=kwargs.pop("ps_range", 4),
+        fast=kwargs.pop("fast", True),
+    )
 
     y = get_y(clip)
     mv = VMDegrain(y, thSAD, prefilter, **kwargs)
 
-    if cuda[1]:
-        bm3dfunc = core.bm3dcuda if not hasattr(core, "bm3dcuda_rtc") else core.bm3dcuda_rtc
+    has_cuda = hasattr(core, "bm3dcuda") or hasattr(core, "bm3dcuda_rtc")
+    has_hip = hasattr(core, "bm3dhip")
+
+    if cuda[1] and (has_cuda or has_hip):
+        if has_cuda:
+            bm3dfunc = core.bm3dcuda if not hasattr(core, "bm3dcuda_rtc") else core.bm3dcuda_rtc
+        else:
+            bm3dfunc = core.bm3dhip
     else:
         bm3dargs.pop("fast")
         bm3dfunc = core.bm3dcpu
@@ -100,5 +131,5 @@ def schizo_denoise(src: vs.VideoNode, sigma: float | list[float] = [0.8, 0.3], t
     out = join(depth(bm3d, 16), nlm)
     out = depth(out, get_depth(src))
     if csharp != False:
-        out = contrasharpening(out, src, mode = 3 if csharp == True else csharp)
+        out = contrasharpening(out, src, mode=3 if csharp == True else csharp)
     return out
