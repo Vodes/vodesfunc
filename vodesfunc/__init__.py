3242589dc835a033adb2031c6a77cf580560d05f
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-04-06 16:42:09 +0200

    Remove descale module (DescaleTarget)
    It's time to say goodbye. Use RescaleBuilder.

--- vodesfunc/__init__.py
+++ vodesfunc/__init__.py
@@ -4,13 +4,12 @@ Oh god no
 
 # flake8: noqa
 
-from . import aa, misc, noise, scale, denoise, descale, rescale, util, spikefinder, rescale_ext
+from . import aa, misc, noise, scale, denoise, rescale, util, spikefinder, rescale_ext
 from .aa import *
 from .misc import *
 from .noise import *
 from .scale import *
 from .rescale import *
-from .descale import *
 from .denoise import *
 from .util import *
 from .spikefinder import *
