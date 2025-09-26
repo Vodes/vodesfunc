16f8153bc66fe229f095aab66f30ed8295ddfe67
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-09-27 01:31:39 +0200

    This might work as intended?

--- vodesfunc/rescale_ext/regular_rescale.py
+++ vodesfunc/rescale_ext/regular_rescale.py
@@ -42,20 +42,23 @@ class RescBuildNonFB(RescaleBase):
             if not callable(self.ignore_mask):
                 self.ignore_mask = border_clipping_mask
 
-            def _descale(clip: vs.VideoNode, sc_args: ScalingArgs, order: str) -> vs.VideoNode:
-                ignore_mask = self.ignore_mask(clip, sc_args, self.kernel, BorderHandling(self.border_handling))  # type:ignore[reportCallableArgument]
+            def _descale_step(workclip: vs.VideoNode, sc_args: ScalingArgs, order: str) -> vs.VideoNode:
+                sc_args_direction = ScalingArgs.from_args(
+                    clip, height=height, width=width, base_height=base_height, base_width=base_width, src_top=shift[0], src_left=shift[1], mode=order
+                )
+                ignore_mask = self.ignore_mask(workclip, sc_args_direction, self.kernel, BorderHandling(self.border_handling))  # type:ignore[reportCallableArgument]
                 direction = {"width" if order == "w" else "height": out_width if order == "w" else out_height}
 
                 return self.kernel.descale(
-                    clip,
-                    **(sc_args.kwargs() | dict(border_handling=self.border_handling, ignore_mask=ignore_mask) | direction),
+                    workclip,
+                    **(sc_args_direction.kwargs() | dict(border_handling=self.border_handling, ignore_mask=ignore_mask) | direction),
                 )
 
             self.descaled = clip
             out_width, out_height = sc_args.width, sc_args.height
 
             for order in mode.lower():
-                self.descaled = _descale(self.descaled, sc_args, order)
+                self.descaled = _descale_step(self.descaled, sc_args, order)
         else:
             self.descaled = self.kernel.descale(clip, **args)
 
