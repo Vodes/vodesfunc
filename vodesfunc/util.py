7b7a2fd383967647f3dcbaffc06401bb3eb11cbc
Alex H. <48185123+Vodes@users.noreply.github.com>
2026-04-05 14:14:02 +0200

    out: Get it to work with vsview too

--- vodesfunc/util.py
+++ vodesfunc/util.py
@@ -2,6 +2,9 @@ from jetpytools import KwargsT
 from vstools import vs, core
 
 from functools import partial
+from vsmuxtools import src_file
+
+from contextlib import suppress
 
 
 __all__: list[str] = [
@@ -11,29 +14,56 @@ __all__: list[str] = [
 
 
 def set_output(
-    clip: vs.VideoNode, name: str | None = None, frame_info: bool = False, allow_comp: bool = True, cache: bool | None = None, **kwargs: KwargsT
+    clip: vs.VideoNode | src_file,
+    name: str | None = None,
+    frame_info: bool = False,
+    allow_comp: bool = True,
+    cache: bool | None = None,
+    **kwargs: KwargsT,
 ) -> vs.VideoNode:
     """
     Outputs a clip. Less to type.
-    Designed to be used with the good ol 'from vodesfunc import *' and the 'out' alias
+    If you're using vsview, cache is irrelevant and allow_comp is unfortunately not a thing right now.
     """
+    if isinstance(clip, src_file):
+        clip = clip.src_cut
+
     if frame_info and name:
         clip = _print_frameinfo(clip, name)
 
-    try:
-        args = KwargsT(name=name, cache=cache, disable_comp=not allow_comp)
-        if kwargs:
-            args.update(**kwargs)
-        from vspreview import set_output as setsu_sucks
-        from vspreview.api import is_preview
+    is_preview_vsview = False
+    with suppress(ImportError):
+        from vsview import is_preview as vsv_is_preview
 
-        if cache is None:
-            cache = is_preview()
-        setsu_sucks(clip, **args)
-    except:
+        is_preview_vsview = vsv_is_preview()
+
+        if is_preview_vsview:
+            from vsview import set_output as vsv_out
+
+            vsv_out(clip, name, **kwargs)
+            return clip
+
+    is_preview_vspreview = False
+    with suppress(ImportError):
+        from vspreview.api import is_preview as vsp_is_preview
+
+        is_preview_vspreview = vsp_is_preview()
+
+        if is_preview_vspreview:
+            from vspreview.api import set_output as vsp_out
+
+            args = KwargsT(name=name, cache=cache or True, disable_comp=not allow_comp)
+            if kwargs:
+                args.update(**kwargs)
+            vsp_out(clip, **args)
+            return clip
+
+    if not is_preview_vspreview and not is_preview_vsview:
         if name is not None:
             clip = clip.std.SetFrameProp("Name", data=name)
+
         clip.set_output(len(vs.get_outputs()))
+
     return clip
 
 
