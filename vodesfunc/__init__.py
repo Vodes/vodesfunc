6cfbab21c835a751a3672c14755f58f2a88b644f
Alex H. <48185123+Vodes@users.noreply.github.com>
2026-05-13 21:53:40 +0200

    pyproject: Add some dev deps and dynamic versioning

--- vodesfunc/__init__.py
+++ vodesfunc/__init__.py
@@ -2,6 +2,15 @@
 Oh god no
 """
 
+__version__: str
+__version_tuple__: tuple[int | str, ...]
+
+try:
+    from ._version import __version__, __version_tuple__
+except ImportError:
+    __version__ = "0.0.0+unknown"
+    __version_tuple__ = (0, 0, 0, "+unknown")
+
 # flake8: noqa
 
 from . import misc, noise, scale, denoise, rescale, util, spikefinder, rescale_ext
